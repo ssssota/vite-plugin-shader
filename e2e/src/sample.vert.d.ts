a9049cd665776c7b9e54472d4d6c33025db0152e
ssssota <tommysouta@gmail.com>
2025-05-17 12:46:34 +0900

    chore: fmt

--- e2e/src/sample.vert.d.ts
+++ e2e/src/sample.vert.d.ts
@@ -1,15 +1,33 @@
-import { mappings, _getAttribLocation, _getUniformLocation } from "virtual:vite-plugin-shader/runtime";
+import {
+	_getAttribLocation,
+	_getUniformLocation,
+	mappings,
+} from "virtual:vite-plugin-shader/runtime";
 type Variables = "a_position" | "outColor";
-export const source = "#version 300 es\nin vec4 v;void main(){gl_Position=v;}" as const;
+export const source =
+	"#version 300 es\nin vec4 v;void main(){gl_Position=v;}" as const;
 
-type ShaderType = WebGLRenderingContextBase["VERTEX_SHADER"] | WebGLRenderingContextBase["FRAGMENT_SHADER"];
-export const createShaderWithSource = (gl: WebGLRenderingContextBase, type: ShaderType): WebGLShader | null => {
-  const shader = gl.createShader(type);
-  if (!shader) return null;
-  gl.shaderSource(shader, source);
+type ShaderType =
+	| WebGLRenderingContextBase["VERTEX_SHADER"]
+	| WebGLRenderingContextBase["FRAGMENT_SHADER"];
+export const createShaderWithSource = (
+	gl: WebGLRenderingContextBase,
+	type: ShaderType,
+): WebGLShader | null => {
+	const shader = gl.createShader(type);
+	if (!shader) return null;
+	gl.shaderSource(shader, source);
 	return shader;
-}
-type GetAttribLocation = (gl: WebGLRenderingContextBase, program: WebGLProgram, name: Variables) => GLint;
+};
+type GetAttribLocation = (
+	gl: WebGLRenderingContextBase,
+	program: WebGLProgram,
+	name: Variables,
+) => GLint;
 export const getAttribLocation: GetAttribLocation = _getAttribLocation;
-type GetUniformLocation = (gl: WebGLRenderingContextBase, program: WebGLProgram, name: Variables) => WebGLUniformLocation | null;
+type GetUniformLocation = (
+	gl: WebGLRenderingContextBase,
+	program: WebGLProgram,
+	name: Variables,
+) => WebGLUniformLocation | null;
 export const getUniformLocation: GetUniformLocation = _getUniformLocation;
